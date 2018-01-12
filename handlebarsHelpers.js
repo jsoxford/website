c5fee97ef3b6a94638779e49bcf16953a59b93a3
Marcus Noble <marcus@marcusnoble.co.uk>
2018-01-12 06:49:53 +0000

    Add a sponsors page

--- handlebarsHelpers.js
+++ handlebarsHelpers.js
@@ -44,5 +44,15 @@ module.exports = {
     });
 
     return Object.values(speakers);
+  },
+  'getSponsors': function(events) {
+    const sponsors = {}
+    events.forEach(event => {
+      event.sponsors.forEach(sponsor => {
+        sponsors[sponsor.name] = sponsor;
+      });
+    });
+
+    return Object.values(sponsors);
   }
 }
